b5588479e3ec984078e13f7fc83a7a2ccb8498ef
asone1 <32952066+asone1@users.noreply.github.com>
2020-05-14 09:18:38 +0800

    Create DS_Depart_Site.java

---
+++ AdminJob/src/customizedReport/DS_Depart_Site.java
@@ -0,0 +1,17 @@
+package customizedReport;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class DS_Depart_Site {
+	String[] power = { "電源", "TW3", "JL26" };
+	String[] magne = { "電磁", "TW2", "JL2" };
+	String[] ICP = { "資通", "TW4", "JL27" };
+	String[] TW_LCP = { "光通訊", "TW5", "JL5" };
+	String[] JL_LCP = { "光電", "JL7" };
+
+    String[][] allDepart = {power, magne, ICP, TW_LCP, JL_LCP};
+
+
+}
